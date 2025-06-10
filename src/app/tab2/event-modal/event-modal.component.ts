d9051b097828744c117fb1c885e3e4ea0b79a012
GabMer <ana.gabrielucb.edu.bo>
2025-06-09 20:25:46 -0400

    modals

---
+++ src/app/tab2/event-modal/event-modal.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { ModalController, IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+    selector: 'app-event-modal',
+    standalone: true,
+    imports: [CommonModule, IonicModule, ReactiveFormsModule],
+    templateUrl: './event-modal.component.html',
+    styleUrls: ['./event-modal.component.scss'],
+})
+export class EventModalComponent {
+    eventForm: FormGroup;
+
+    constructor(
+        private modalCtrl: ModalController,
+        private fb: FormBuilder
+    ) {
+        this.eventForm = this.fb.group({
+            title: ['', Validators.required],
+            start: ['', Validators.required],
+            end: ['', Validators.required],
+            description: [''],
+            color: ['#3788d8'],
+            completed: [false]
+        });
+    }
+
+    cancel() {
+        this.modalCtrl.dismiss(null, 'cancel');
+    }
+
+    confirm() {
+        if (this.eventForm.valid) {
+            this.modalCtrl.dismiss(this.eventForm.value, 'confirm');
+        }
+    }
+}
